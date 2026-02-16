a12596b3b0e13626d769ff3802fe45f36e611538
illya <illyakovalskij@gmail.com>
2026-02-16 11:49:47 +0200

    Initial backend setup Add user and movie modules Add list and login modules Add rating and review modules Add admin modules Add new admin modules

--- frontend/src/api/profile.api.ts
+++
@@ -1,6 +0,0 @@
-import api from "./axios";
-
-export const getProfile = () => api.get("/users/me");
-
-export const updateProfile = (data: { nickname?: string; password?: string }) =>
-  api.put("/users/me", data);
--- frontend/src/api/search.api.ts
+++
@@ -1,4 +0,0 @@
-import api from "./axios";
-
-export const searchMovies = (query: string) =>
-  api.get("/movies/search", { params: { q: query } });
